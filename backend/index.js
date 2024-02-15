a63d4bd242fbf1d138899e8f5023bd70c8290cbc
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-15 12:41:18 +0530

    added post method

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,29 @@
 // write basic express boilerplate code,
 // with express.json() middleware;
 
-
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/todo",function(req,res){
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "you have sent the wrong inputs",
+    });
+    return;
+  }
+  //put it on mongodb
+});
 
-})
+app.get("/todos", function (req, res) {});
 
-app.get("/todos",function(req,res){
-
-})
-
-app.put("/completed",function(req,res){
-
-})
-app.listen(port,function(){
-    console.log(`app listening on ${port}`)
-})
+app.put("/completed", function (req, res) {});
+app.listen(port, function () {
+  console.log(`app listening on ${port}`);
+});
