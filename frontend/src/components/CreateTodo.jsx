5286067bdea9c17770d54a7f0bf554c7e6f05d90
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-15 20:39:05 +0530

    created frontend file

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,17 @@
+
+
+
+function CreateTodo(){
+
+    return(
+        <div>
+        <input type="text" placeholder="title" />
+        <br/>
+        <input type="text" placeholder="description" />
+        <br/>
+        <button>Add a Todo</button>
+        </div>
+    )
+}
+
+export default CreateTodo;
