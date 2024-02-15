39aa9eeda408893a80019dadbc0a7390d3a7dc19
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-15 13:04:24 +0530

    create db.js a mongoose schema

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+/**
+ * Todo {
+ * title: string,
+ * description: string,
+ * completed: boolean
+ * }
+ */
+
+
+const mongoose = require("mongoose");
+//mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos  
+//.env file
+mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos")
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: boolean
+})
+
+const todo = mongoose.model("todos", todoSchema)  
+
+module.exports = {
+todo
+}
--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,13 @@ const port = 3000;
 
 app.use(express.json());
 
+/**
+ * we have created a http server;
+ * we have added a right set of validation;
+ * at last creating a mongodb schema
+ * actually putting something in mongodb,getting something lastly updating;
+ */
+
 app.post("/todo", function (req, res) {
   const createPayload = req.body;
   const parsePayload = createTodo.safeParse(createPayload);
@@ -21,7 +28,9 @@ app.post("/todo", function (req, res) {
   //put it on mongodb
 });
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", function (req, res) {
+    
+});
 
 app.put("/completed", function (req, res) {
     const updatePayload = req.body;
